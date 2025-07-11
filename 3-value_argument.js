724772ee05e8f0d62b4799d46f8020e224a070b5
Gelagay G. <gela.sirta3@gmail.com>
2025-07-11 20:57:08 +0300

    Updated value argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -4,8 +4,6 @@ let input = process.argv[2]
 
 if(process.argv[2] === undefined){
     console.log('No argument')
-} else if(process.argv[3] === undefined){
-    console.log('Argument found')
-}else{
-    console.log('Argument found')
+} else{
+    console.log(input)
 }
