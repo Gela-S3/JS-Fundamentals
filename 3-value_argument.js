c63b0c7ce202841ca3430c1d39de71733f93f797
Gelagay G. <gela.sirta3@gmail.com>
2025-07-11 20:41:05 +0300

    Value argument is added

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,8 +2,10 @@ let process = require('process')
 
 let input = process.argv[2]
 
-if(process.argv.length - 2 == 0){
+if(process.argv[2] === undefined){
     console.log('No argument')
-} else{
-    console.log(input)
+} else if(process.argv[3] === undefined){
+    console.log('Argument found')
+}else{
+    console.log('Argument found')
 }
