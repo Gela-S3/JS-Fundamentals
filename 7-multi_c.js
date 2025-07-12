f17b6f07c70b1a4788a65db22a1c6d63e4fbe69d
Gelagay G. <gela.sirta3@gmail.com>
2025-07-12 23:38:02 +0300

    Multilanguage print fixed

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,7 +2,8 @@ const x = parseInt(process.argv[2]);
 
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
-} else if (x > 0) {
-  for (let i = 0; i < x; i++) {
+} 
+else if (x > 0) {
+    for (let i = 0; i < x; i++)
     console.log('C is fun');
-  }
+} 
